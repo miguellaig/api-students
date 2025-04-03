03dc85f727ee9053376ff1f20fd5d38e9410809a
Miguel Laignier <miguelrgit@gmail.com>
2025-04-03 14:35:07 -0300

    Modificações no db

---
+++ db/db.go
@@ -0,0 +1,47 @@
+package db
+
+import (
+	"fmt"
+	"log"
+
+	"gorm.io/driver/sqlite" // Sqlite driver based on CGO
+
+	// "github.com/glebarez/sqlite" // Pure go SQLite driver, checkout https://github.com/glebarez/sqlite for details
+	"gorm.io/gorm"
+)
+
+type Student struct {
+	gorm.Model
+	Name   string
+	CPF    int
+	Email  string
+	Age    int
+	Active bool
+}
+
+func Init() *gorm.DB {
+	db, err := gorm.Open(sqlite.Open("student.db"), &gorm.Config{})
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	db.AutoMigrate(&Student{})
+
+	return db
+}
+
+func AddStudent() {
+	db := Init()
+
+	student := Student{
+		Name:   "Bento",
+		CPF:    12345,
+		Email:  "bento@gmail.com",
+		Age:    4,
+		Active: true,
+	}
+	if result := db.Create(&student); result.Error != nil {
+		fmt.Println("Error to create student")
+	}
+	fmt.Println("Create student!")
+}
